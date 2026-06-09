6603bfe9f96d7ab4c6d9e9646ec781d78a317bde
Fondante-GGG <grazy9891@gmail.com>
2026-06-09 18:54:23 +0600

    initial commit

---
+++ app/settings/views.py
@@ -0,0 +1,33 @@
+from django.views.generic import (
+    ListView,
+    CreateView,
+    RetrieveView,
+    UpdateView,
+    DeleteView
+)
+from django.urls import reverse_lazy
+
+from .models import Course
+
+
+class CourseList(ListView):
+    model = Course
+    template_name = "courses/list.html"
+    context_object_name = "courses"
+
+class CourseCreate(CreateView):
+    model = Course
+    template_name = "courses/create.html"
+    fields = "__all__"
+    success_url = reverse_lazy("list")
+
+class CourseUpdate(UpdateView):
+    model = Course
+    template_name = "courses/update.html"
+    fields = "__all__"
+    success_url = reverse_lazy("list")
+
+class CourseDelete(DeleteView):
+    model = Course
+    template_name = "courses/delete.html"
+    success_url = reverse_lazy("list")
